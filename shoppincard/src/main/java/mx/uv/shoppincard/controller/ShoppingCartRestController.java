b70b475c11a869277edcc3cec61bd45c9daa0295
RubenRiveraMorales <89487755+RubenRiveraMorales@users.noreply.github.com>
2023-06-01 16:28:13 -0600

    Add files via upload

--- shoppincard/src/main/java/mx/uv/shoppincard/controller/ShoppingCartRestController.java
+++ shoppincard/src/main/java/mx/uv/shoppincard/controller/ShoppingCartRestController.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,7 +27,7 @@ import mx.uv.shoppincard.service.CustomerService;
 import mx.uv.shoppincard.service.OrderService;
 import mx.uv.shoppincard.service.ProductService;
 
-
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api")
 public class ShoppingCartRestController {
